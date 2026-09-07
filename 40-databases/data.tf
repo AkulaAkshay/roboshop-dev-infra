0a3b1620bec0d0109e0f81836171d4c70fda3327
Akula Akshay <akshayakula13@gmail.com>
2026-09-07 11:05:00 +0530

    pr

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -1,19 +1,20 @@
 data "aws_ami" "joindevops" {
-    owners           = ["973714476881"]
-    most_recent      = true
-    
+
+    owners = ["973714476881"]
+    most_recent = true
+
     filter {
-        name   = "name"
-        values = ["RHEL-9-DevOps-Practice"]
+        name = "name"
+        values = ["Redhat-9-DevOps-Practice"]
     }
 
     filter {
-        name   = "root-device-type"
+        name = "root-device-type"
         values = ["ebs"]
     }
 
     filter {
-        name   = "virtualization-type"
+        name = "virtualization-type"
         values = ["hvm"]
     }
 }
