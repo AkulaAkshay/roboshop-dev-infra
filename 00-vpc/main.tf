a44ce9880044113bd80d77bc7115cccd7d2b1139
Akula Akshay <akshayakula13@gmail.com>
2026-09-07 10:58:15 +0530

    pr

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "git::https://github.com/AkulaAkshay/roboshop-dev-infra.git?ref=main"
+    source = "git::https://github.com/AkulaAkshay/terraform-aws-vpc.git?ref=main"
     # VPC
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
