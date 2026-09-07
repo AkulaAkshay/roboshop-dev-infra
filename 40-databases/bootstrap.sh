8afe7e1228016c93cdb94dc1f92944aaac30f83b
Akula Akshay <akshayakula13@gmail.com>
2026-09-07 11:23:12 +0530

    pr

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,4 +2,4 @@
 
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/AkulaAkshay/ansible-roboshop-roles-tf.git -e component=${{component}} main.yaml
+ansible-pull -U https://github.com/AkulaAkshay/ansible-roboshop-roles-tf.git -e component=$component main.yaml
