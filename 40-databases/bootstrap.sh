c0eb2e3c26919908f1699a8decc08860971177cc
Akula Akshay <akshayakula13@gmail.com>
2026-09-08 10:22:07 +0530

    tf-pr

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 component=$1
+
 dnf install ansible -y
-# ansible-pull is not giving respect to inventory.ini, even though by default it is connecting lo localhost, we are getting warning sign . so, lets use another approach
-#ansible-pull -U https://github.com/AkulaAkshay/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+#ansible-pull -U https://github.com/daws-86s/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+# git clone ansible-playbook
+# cd ansible-playbook
+# ansible-playbook -i inventory main.yaml
 
-REPO_URL=https://github.com/AkulaAkshay/ansible-roboshop-roles-tf.git
-REPO_DIR=opt/roboshop/ansible
+REPO_URL=https://github.com/daws-86s/ansible-roboshop-roles-tf.git
+REPO_DIR=/opt/roboshop/ansible
 ANSIBLE_DIR=ansible-roboshop-roles-tf
-
 
 mkdir -p $REPO_DIR
 mkdir -p /var/log/roboshop/
@@ -16,15 +18,15 @@ touch ansible.log
 
 cd $REPO_DIR
 
-#check if ansible repo is already cloned or not
+# check if ansible repo is already cloned or not
+
 if [ -d $ANSIBLE_DIR ]; then
 
     cd $ANSIBLE_DIR
     git pull
 else
-    
     git clone $REPO_URL
     cd $ANSIBLE_DIR
 fi
 
-ansile playbook -e component=$component main.yaml
+ansible-playbook -e component=$component main.yaml
