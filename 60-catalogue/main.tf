6770f8914b8044f06a40847450ffd233e8fc0f38
Akula Akshay <akshayakula13@gmail.com>
2026-09-14 11:06:42 +0530

    auto scaling

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -61,6 +61,129 @@ resource "aws_ami_from_instance" "catalogue" {
   )
 }
 
+# creating target group, so that once the auto scaling group create the instance it will deploy those instances in a,particular target group
+resource "aws_lb_target_group" "catalogue" {
+  name     = "${local.common_name_suffix}-catalogue"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+  deregistration_delay = 60 # waiting period before deleting the instance, inorder to serve the already existing traffic in the server (when company want to rellease an employee its like notice period -> in order to complete the current roles and responsibilities) - for big applications we use 120-360
 
+  health_check {
+    healthy_threshold = 2
+    interval = 10
+    matcher = "200-299"
+    path = "/health"
+    port = 8080
+    protocol = "HTTP"
+    timeout = 2
+    unhealthy_threshold = 2
+  }
+}
 
+# launch template - details of (vpc, subnet,sg, instance type, and ami - already configured instance)
+resource "aws_launch_template" "catalogue" {
+  name = "${local.common_name_suffix}-catalogue"
+  image_id = aws_ami_from_instance.catalogue.id
+
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [local.catalogue_sg_id]
+
+  # when we run terraform apply again, a new version will be created with new AMI ID
+  update_default_version = true
+
+  # tags attached to the instance
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name_suffix}-catalogue"
+      }
+    )
+  }
+
+  # tags attached to the volume created by instance
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name_suffix}-catalogue"
+      }
+    )
+  }
+
+  # tags attached to the launch template
+  tags = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name_suffix}-catalogue"
+      }
+  )
+
+}
+
+# auto scaling group
+resource "aws_autoscaling_group" "catalogue" {
+  name                      = "${local.common_name_suffix}-catalogue"
+  max_size                  = 10
+  min_size                  = 1
+  health_check_grace_period = 100
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = false
+  launch_template {
+    id      = aws_launch_template.catalogue.id
+    version = aws_launch_template.catalogue.latest_version
+  }
+  vpc_zone_identifier       = local.private_subnet_ids
+  target_group_arns = [aws_lb_target_group.catalogue.arn]
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50 # atleast 50% of the instances should be up and running
+    }
+    triggers = ["launch_template"]
+  }
+  
+  dynamic "tag" {  # we will get the iterator with name as tag
+    for_each = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name_suffix}-catalogue"
+      }
+    )
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+
+}
+
+# auto scaling policy
+resource "aws_autoscaling_policy" "catalogue" {
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
+  name                   = "${local.common_name_suffix}-catalogue"
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 75.0
+  }
+}
 
