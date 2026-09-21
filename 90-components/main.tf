021c73182125739f6e547758f8bcc5937e1a7822
Akula Akshay <akshayakula13@gmail.com>
2026-09-21 11:12:19 +0530

    pr

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,7 +1,7 @@
 # module "components" {
 #     source = "../../terraform-roboshop-component"
 #     component = var.component
-#     rule_priority = var.rule_priority
+#     rule_priority = var.rule_priority..
 # }
 
 
