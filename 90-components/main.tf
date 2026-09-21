25dd356762c5e6ca0ce69100aa4f2740dbf82a30
Akula Akshay <akshayakula13@gmail.com>
2026-09-21 10:23:02 +0530

    pr

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,5 +1,5 @@
 module "components" {
     source = "git::https://github.com/AkulaAkshay/terraform-roboshop-component.git?ref=main"
     component = var.component
-    rule_priority = var.rule_priority
+    rule_priority = 10
 }
